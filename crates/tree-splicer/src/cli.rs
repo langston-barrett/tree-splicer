e75d6e322db00cda354a9d54c8e19155bb5c545f
Antonin Delpeuch <antonin@delpeuch.eu>
2025-07-02 17:06:10 +0200

    Address clippy's complaints

--- crates/tree-splicer/src/cli.rs
+++ crates/tree-splicer/src/cli.rs
@@ -109,13 +109,13 @@ pub struct Args {
 }
 
 fn read_file(file: &str) -> Result<String> {
-    fs::read_to_string(file).with_context(|| format!("Failed to read file {}", file))
+    fs::read_to_string(file).with_context(|| format!("Failed to read file {file}"))
 }
 
 fn parse(language: &tree_sitter::Language, code: &str) -> Result<tree_sitter::Tree> {
     let mut parser = tree_sitter::Parser::new();
     parser
-        .set_language(language.into())
+        .set_language(language)
         .context("Failed to set tree-sitter parser language")?;
     parser.parse(code, None).context("Failed to parse code")
 }
